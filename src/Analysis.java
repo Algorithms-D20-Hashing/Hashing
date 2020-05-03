c19df3b497f23fad873af6f24760f83df68ed287
Arun <sun.chintalapati@gmail.com>
2020-05-03 16:57:16 -0400

    Record times to get each element of hash table

--- src/Analysis.java
+++ src/Analysis.java
@@ -10,6 +10,7 @@ public class Analysis {
 		LinkedList<Double> quarterDictTimes = new LinkedList<Double>();
 		LinkedList<Double> eigthDictTimes = new LinkedList<Double>();
 		
+		System.out.println("Fill times");
 		
 		//Fill full dictionary. Mult Factor: x8
 		fullDictTimes.add(fill(8, 321165));
@@ -99,11 +100,109 @@ public class Analysis {
 		eigthDictTimes.add(fill(1.125, 40145));
 		
 		//Print times
-		System.out.println(eigthDictTimes);
+		System.out.println(eigthDictTimes + "\n\n\n");
+		
+		
+		
+		LinkedList<Double> fullDictGetTimes = new LinkedList<Double>();
+		LinkedList<Double> halfDictGetTimes = new LinkedList<Double>();
+		LinkedList<Double> quarterDictGetTimes = new LinkedList<Double>();
+		LinkedList<Double> eigthDictGetTimes = new LinkedList<Double>();
+		
+		System.out.println("Get times");
+		
+		//Fill full dictionary. Mult Factor: x8
+		fullDictGetTimes.add(testGet(8, 321165));
+		
+		//testGet full dictionary. Mult Factor: x4
+		fullDictGetTimes.add(testGet(4, 321165));
+		
+		//testGet full dictionary. Mult Factor: x2
+		fullDictGetTimes.add(testGet(2, 321165));
+		
+		//testGet full dictionary. Mult Factor: x1.5
+		fullDictGetTimes.add(testGet(1.5, 321165));
+		
+		//testGet full dictionary. Mult Factor: x1.25
+		fullDictGetTimes.add(testGet(1.25, 321165));
+		
+		//testGet full dictionary. Mult Factor: x1.125
+		fullDictGetTimes.add(testGet(1.125, 321165));
+		
+		//Print times
+		System.out.println(fullDictGetTimes);
+		
+		
+		
+		//testGet half dictionary. Mult Factor: x8
+		halfDictGetTimes.add(testGet(8, 160582));
+		
+		//testGet half dictionary. Mult Factor: x4
+		halfDictGetTimes.add(testGet(4, 160582));
+		
+		//testGet half dictionary. Mult Factor: x2
+		halfDictGetTimes.add(testGet(2, 160582));
+		
+		//testGet half dictionary. Mult Factor: x1.5
+		halfDictGetTimes.add(testGet(1.5, 160582));
+		
+		//testGet half dictionary. Mult Factor: x1.25
+		halfDictGetTimes.add(testGet(1.25, 160582));
+		
+		//testGet half dictionary. Mult Factor: x1.125
+		halfDictGetTimes.add(testGet(1.125, 160582));
+		
+		//Print times
+		System.out.println(halfDictGetTimes);
+		
+		
+		
+		//testGet quarter dictionary. Mult Factor: x8
+		quarterDictGetTimes.add(testGet(8, 80291));
+		
+		//testGet quarter dictionary. Mult Factor: x4
+		quarterDictGetTimes.add(testGet(4, 80291));
+		
+		//testGet quarter dictionary. Mult Factor: x2
+		quarterDictGetTimes.add(testGet(2, 80291));
+		
+		//testGet quarter dictionary. Mult Factor: x1.5
+		quarterDictGetTimes.add(testGet(1.5, 80291));
+		
+		//testGet quarter dictionary. Mult Factor: x1.25
+		quarterDictGetTimes.add(testGet(1.25, 80291));
+		
+		//testGet quarter dictionary. Mult Factor: x1.125
+		quarterDictGetTimes.add(testGet(1.125, 80291));
+		
+		//Print times
+		System.out.println(quarterDictGetTimes);
+		
+		
+		
+		//testGet eigth dictionary. Mult Factor: x8
+		eigthDictGetTimes.add(testGet(8, 40145));
+		
+		//testGet eigth dictionary. Mult Factor: x4
+		eigthDictGetTimes.add(testGet(4, 40145));
+		
+		//testGet eigth dictionary. Mult Factor: x2
+		eigthDictGetTimes.add(testGet(2, 40145));
+		
+		//testGet eigth dictionary. Mult Factor: x1.5
+		eigthDictGetTimes.add(testGet(1.5, 40145));
+		
+		//testGet eigth dictionary. Mult Factor: x1.25
+		eigthDictGetTimes.add(testGet(1.25, 40145));
+		
+		//testGet eigth dictionary. Mult Factor: x1.125
+		eigthDictGetTimes.add(testGet(1.125, 40145));
+		
+		//Print times
+		System.out.println(eigthDictGetTimes);
 		
 		
 	}
-	
 	
 	//Fills hash table with specified number of values with a specified multiplicative factor for resizing
 	public static double fill(double multFactor, int fillLength) throws IOException {
@@ -115,4 +214,21 @@ public class Analysis {
 		}
 		return watch.elapsedTime();
 	}
+	
+	public static double testGet(double multFactor, int fillLength) throws IOException {
+		LinearProbingHashST hashST = new LinearProbingHashST(0, multFactor);
+		EnglishDictionaryExtractor ede = new EnglishDictionaryExtractor();
+		for(int i = 0; i < fillLength; i++) {
+			hashST.put(ede.next(), 1);
+		}
+		
+		ede = new EnglishDictionaryExtractor();
+		
+		Stopwatch watch = new Stopwatch();
+		for(int i = 0; i < fillLength; i++) {
+			hashST.get(ede.next());
+		}
+		return watch.elapsedTime();
+	}
+
 }
