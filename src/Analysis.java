a9800e513783c643a2c81c74bde7247fecb9fb87
Arun <sun.chintalapati@gmail.com>
2020-05-03 14:59:48 -0400

    Add english dictionary to project

--- src/Analysis.java
+++ src/Analysis.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
 
 public class Analysis {
 	
 	
 	public static void main(String [] args) {
+		LinkedList<Double> times = new LinkedList<Double>();
+		
 		Stopwatch watch = new Stopwatch();
+		
 	}
 }
